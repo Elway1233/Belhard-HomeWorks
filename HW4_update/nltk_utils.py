0023a2b882054117d0bf0dd390e61d97ced28719
Elway <47496682+Elway1233@users.noreply.github.com>
2026-03-19 21:55:33 +0300

    Update nltk_utils.py

--- HW4_update/nltk_utils.py
+++ HW4_update/nltk_utils.py
@@ -1,7 +1,7 @@
+import difflib
+
 import numpy as np
 import nltk
-import pymorphy3
-morph = pymorphy3.MorphAnalyzer()
 from nltk.stem.snowball import SnowballStemmer
 stemmer = SnowballStemmer("russian")
 
@@ -21,25 +21,24 @@ def stem(word):
     words = [stem(w) for w in words]
     -> ["organ", "organ", "organ"]
     """
-    return morph.parse(word.lower())[0].normal_form
+    return stemmer.stem(word.lower())
 
 
 def bag_of_words(tokenized_sentence, words):
-    """
-    return bag of words array:
-    1 for each known word that exists in the sentence, 0 otherwise
-    example:
-    sentence = ["hello", "how", "are", "you"]
-    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
-    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
-    """
-    # stem each word
     sentence_words = [stem(word) for word in tokenized_sentence]
-    # initialize bag with 0 for each word
+
     bag = np.zeros(len(words), dtype=np.float32)
+
     for idx, w in enumerate(words):
-        if w in sentence_words: 
-            bag[idx] = 1
+        for s_word in sentence_words:
+            if w == s_word:
+                bag[idx] = 1.0
+                break
+            elif len(w) > 4 and w in s_word:
+                bag[idx] = 1.0
+                break
+            elif difflib.SequenceMatcher(None, w, s_word).ratio() > 0.8:
+                bag[idx] = 1.0
+                break
 
     return bag
-
